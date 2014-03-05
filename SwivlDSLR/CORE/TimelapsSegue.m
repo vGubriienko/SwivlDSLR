289c5493020d623b043526f6b1a8e589afa9f592
Zhenya Koval <kovalgenya@gmail.com>
2014-03-05 17:17:56 +0200

    Update navigation to settings controllers

--- SwivlDSLR/CORE/TimelapsSegue.m
+++ SwivlDSLR/CORE/TimelapsSegue.m
@@ -7,19 +7,18 @@
 
 #import "TimelapsSegue.h"
 
+#import "SWMainViewController.h"
+
 @implementation TimelapsSegue
 
-- (void)perform {
-    UIViewController *sourceViewController = self.sourceViewController;
+- (void)perform
+{
+    SWMainViewController *sourceViewController = self.sourceViewController;
     UIViewController *destinationViewController = self.destinationViewController;
-    CGRect _destinationFrame = CGRectMake(0, 0, 406, 320);
-    // Add the destination view as a subview
-    [sourceViewController.view addSubview:destinationViewController.view];
-    destinationViewController.view.frame = _destinationFrame;
-    
-    _destinationFrame = destinationViewController.view.frame;
-     NSLog(@"Frame = %@", NSStringFromCGRect(_destinationFrame));
-    
+    CGRect contentViewFrame = sourceViewController.contentView.bounds;
+
+    destinationViewController.view.frame = contentViewFrame;
+    [sourceViewController.contentView addSubview:destinationViewController.view];
 }
 
 @end
